95da3c4e567f98f8bc3bac5fa8e0c4b188bd3b06
YangSeonho <chandler@YangSeonhoui-MacBook-Pro.local>
2016-09-09 05:16:28 +1200

    remove some button and option in  first row in search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,9 @@ class SearchController < ApplicationController
 
   def show
     query_string = make_query params
+
     @papers = Paper.where(query_string)
+
   end
 
 
@@ -17,12 +19,12 @@ class SearchController < ApplicationController
     items = params[:search][:search_fields_attributes].values
     items.each do |search_field|
       unless search_field[:content].blank?
-        query_string << "#{search_field[:operator]} " unless items.first == search_field
-        query_string << "#{search_field[:limiter]} LIKE "
+        query_string << "#{search_field[:operator]} " unless query_string.blank?
+        query_string << "#{search_field[:limiter]} ILIKE "
         query_string << "'%#{search_field[:content]}%' "
       end
     end
-    puts "quering #{query_string}"
+    puts "quering = #{query_string}"
     query_string
   end
 end
