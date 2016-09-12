530bbd2473a89c87ac92cacba72ebe2e4203366b
YangSeonho <chandler@YangSeonhoui-MacBook-Pro.local>
2016-09-13 00:01:04 +1200

    delete test code in paper model

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,9 +3,4 @@ class Paper < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :participants
   has_and_belongs_to_many :research_methods
-
-  # Implement Search Papers
-  scope :search, lambda {|fields,search_text|
-    where(["#{field} LIKE ?","%#{search_text}%"])
-  }
 end
