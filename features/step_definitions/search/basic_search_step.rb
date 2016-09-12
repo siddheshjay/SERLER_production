aad6418fa6eca1e15444ad49db2cfeec7b60349f
Allen Zhong <zhong.allen@gmail.com>
2016-09-12 14:33:48 +1200

    add features and step

---
+++ features/step_definitions/search/basic_search_step.rb
@@ -0,0 +1,20 @@
+#Feature: Search For Empirical Evidence
+  #As a registered user
+  #I want to be able to search for empirical evidence on different methods based on different constraints
+  #So that I can make informed decisions
+
+Given(/^I'm logged in as a user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I'm on search page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I type specific keywords and click search button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get specific results$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
