4571f8caab1c7267f83b766f3dec3748dc9312d9
Sean <yuxiang8931@gmail.com>
2016-09-15 21:44:43 +1200

    Fix Travis CI - add Rsepc and Cucumber

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = ['*.js', '*.css']
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
