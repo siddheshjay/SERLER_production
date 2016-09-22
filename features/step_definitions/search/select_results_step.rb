c894c639a2c35d12d047bb6f37da81437ff1e5ea
isabel930514 <isabella930514@gmail.com>
2016-09-22 11:49:16 +0000

    Initial work select_results for user stories

---
+++ features/step_definitions/search/select_results_step.rb
@@ -0,0 +1,23 @@
+Given(/^I’m logged in as a user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I’m on search result page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^The result is shown according to different columns$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Each column has its own checklist$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the checklist of one column$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can make the column visible or unvisible$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
