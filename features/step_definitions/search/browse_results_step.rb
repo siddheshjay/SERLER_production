22b214de7235cc78bcc09419899d47d3df5e882b
isabel930514 <isabella930514@gmail.com>
2016-09-22 11:17:28 +0000

    Initial work browse_results for user stories

---
+++ features/step_definitions/search/browse_results_step.rb
@@ -0,0 +1,23 @@
+Given(/^I’m logged in as a user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I’m on search page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I search repository by information like date or SE method$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I get related repository$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I type information like date or SE method and click search button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get related repository and I can browse them$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
