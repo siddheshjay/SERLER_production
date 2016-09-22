f11801440de8e93bb0ba1178411d0c5c740d3f27
isabel930514 <isabella930514@gmail.com>
2016-09-22 11:46:43 +0000

    Initial work save_searchresult for user stories

---
+++ features/step_definitions/search/save_searchresult_step.rb
@@ -0,0 +1,19 @@
+Given(/^I’m logged in as a user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I’m on search page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^There is ‘Save’ button on search page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the save button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should save the search into search list$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
