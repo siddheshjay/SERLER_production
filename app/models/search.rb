ede5f8ca0c52ccef9b66ffe77fb5c4554f178fd3
YangSeonho <chandler@YangSeonhoui-MacBook-Pro.local>
2016-09-26 03:26:47 +1300

    Change application.css.scss and migrate searchs table

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,4 @@
 class Search < ApplicationRecord
-  #attr_accessor :name, :date
-  attr_accessor :from_date,:to_date
   has_many :search_fields,:dependent=> :destroy
   accepts_nested_attributes_for :search_fields, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925053243) do
+ActiveRecord::Schema.define(version: 20160925124441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,8 @@ ActiveRecord::Schema.define(version: 20160925053243) do
     t.datetime "updated_at", null: false
     t.string   "name"
     t.date     "saved_on"
+    t.date     "from_date"
+    t.date     "to_date"
   end
 
   create_table "users", force: :cascade do |t|
