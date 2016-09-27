7acd687a122a69e4c70784e07b960647d3bd9632
Allen Zhong <zhong.allen@gmail.com>
2016-09-27 13:34:45 +1300

    finish tabular format

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,42 +1,49 @@
 class SearchController < ApplicationController
+  before_action :set_search, only: [:search]
   skip_before_filter :authenticate_user!
   def search
-    @search = Search.new
-    @search.search_fields.build
-  end
-
-  def show
-    @search = Search.new(search_params)
-
-    if(@search.search_fields.blank?)
-      @search.search_fields.build
-    end
-    query_string = make_query params
-
-    puts query_string
-
-    @results = EvidenceSource.where(query_string)
-
   end
 
   private
-
   def search_params
-    params.require(:search).permit(:from_date,:to_date,:name, :saved_on,
-                                   :search_fields_attributes => [:content,:field,:op1,:op2,:_destroy]);
+    if params.include?(:search)
+      params.require(:search).permit(:from_date,:to_date,:name, :saved_on,
+                                     :search_fields_attributes => [:content,:field,:op1,:op2,:_destroy]);
+    end
+  end
+
+  def set_search
+    if search_params
+      @search = Search.new(search_params)
+
+      if(@search.search_fields.blank?)
+        @search.search_fields.build
+      end
+
+      search_scope = make_query params
+      @results = search_scope.call(EvidenceSource)
+    else
+      @search = Search.new
+      @search.search_fields.build
+      @results = []
+    end
   end
 
   #make query string for PG
   def make_query params
     query_string = String.new
+    query_author = false
     #Geting Date
 
     items = params[:search][:search_fields_attributes].values
     items.each do |search_field|
       unless search_field[:content].blank?
+        field = search_field[:field]
         query_string << "#{search_field[:op1]} " unless query_string.blank?
-        query_string << "#{search_field[:field]} "
+        query_string << "#{field} "
         query_string << "#{search_field[:op2]} "
+
+        query_author = true if field == "name" or field == "name_abbr"
 
         if search_field[:op2] == "=" || search_field[:op2] == ">" || search_field[:op2] == "<"
           query_string << "'#{search_field[:content]}' "
@@ -58,6 +65,14 @@ class SearchController < ApplicationController
       query_string << "'#{params[:search][:to_date]}' "
     end
 
-    query_string
+    search_scope(query_string, query_author)
+  end
+
+  def search_scope(query_string, query_author=false)
+    if query_author
+      -> (model) { model.joins(:evidence_source_authors).where(query_string)}
+    else
+      -> (model) { model.where(query_string) }
+    end
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-
-
-  describe "GET #show" do
-    it "returns http success with Params" do
-      get :show, params:{ "search"=>{"search_fields_attributes"=>{"0"=>{"field"=>"title", "op2"=>"ILIKE", "content"=>"how"}}},"commit"=>"Search"}
-      expect(response).to have_http_status(:success)
-    end
-
-    it "renders the show template" do
-      get :show, params:{ "search"=>{"search_fields_attributes"=>{"0"=>{"field"=>"title", "op2"=>"ILIKE", "content"=>"how"}}},"commit"=>"Search"}
-      expect(response).to render_template("show")
-    end
-  end
-
   describe "GET #search" do
     it "returns http success" do
       get :search
@@ -26,6 +12,14 @@ RSpec.describe SearchController, type: :controller do
       expect(response).to render_template("search")
     end
 
-  end
+    it "returns http success with Params" do
+      get :search, params:{ "search"=>{"search_fields_attributes"=>{"0"=>{"field"=>"title", "op2"=>"ILIKE", "content"=>"how"}}},"commit"=>"Search"}
+      expect(response).to have_http_status(:success)
+    end
 
+    it "renders results if parameters provided" do
+      get :search, params:{ "search"=>{"search_fields_attributes"=>{"0"=>{"field"=>"title", "op2"=>"ILIKE", "content"=>"how"}}},"commit"=>"Search"}
+      expect(response).to render_template("search")
+    end
+  end
 end
