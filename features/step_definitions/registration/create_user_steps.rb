e64c852be61e29da65a501678799bbca52f8cf9a
Sean <yuxiang8931@gmail.com>
2016-09-29 22:20:37 +1300

    a

---
+++ features/step_definitions/registration/create_user_steps.rb
@@ -0,0 +1,60 @@
+=begin
+Feature: Regist new user
+As a new user
+I want a complete form to fill in my details
+and I should receive a verification email about registration of my account and role.
+
+    Scenario: Regist new user
+Given I am not an registed user
+When I visit registration page
+Then I should be able to input my email
+And I should be able to input my password
+But My password should be minimum 6 characters
+And I should be able to input my confirmation of password
+But My confirmation of password should be same with the password
+And I should be able to chose my gender
+And I should be able to input my first name
+And I should be able to input my middle name
+And I should be able to input my last name
+And I should be able to click "submit" button after filling all fields
+=end
+
+Given /^I am not an registed user$/ do
+  pending
+end
+
+When /^I visit registration page&/ do
+  pending
+end
+
+Then /^I should be able to input my email&/ do
+  pending
+end
+
+And /^I should be able to input my password&/ do
+  pending
+end
+
+And /^I should be able to input my first name&/ do
+  pending
+end
+
+And /^I should be able to input my middle name&/ do
+  pending
+end
+
+But /^My confirmation of password should be same with the password&/ do
+  pending
+end
+
+And /^I should be able to chose my gender&/ do
+  pending
+end
+
+And /^I should be able to input my last name&/ do
+  pending
+end
+
+And /^I should be able to click "submit" button after filling all fields&/ do
+  pending
+end
