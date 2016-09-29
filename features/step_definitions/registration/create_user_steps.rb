1d7f6b355c8d5e0cf8605949d15077afdd18ab49
Sean <yuxiang8931@gmail.com>
2016-09-29 22:35:33 +1300

    add steps

--- features/step_definitions/registration/create_user_steps.rb
+++ features/step_definitions/registration/create_user_steps.rb
@@ -20,41 +20,49 @@ And I should be able to click "submit" button after filling all fields
 =end
 
 Given /^I am not an registed user$/ do
+  visit user_session_path
+end
+
+When /^I visit registration page$/ do
+  click_link "Sign up"
+end
+
+Then /^I should be able to input my email$/ do
+  fill_in "Title", :with => "Caddyshack"
+end
+
+And /^I should be able to input my password$/ do
   pending
 end
 
-When /^I visit registration page&/ do
+But /^My password should be minimum 6 characters$/ do
   pending
 end
 
-Then /^I should be able to input my email&/ do
+And /^I should be able to input my confirmation of password$/ do
   pending
 end
 
-And /^I should be able to input my password&/ do
+But /^My confirmation of password should be same with the password$/ do
   pending
 end
 
-And /^I should be able to input my first name&/ do
+And /^I should be able to chose my gender$/ do
   pending
 end
 
-And /^I should be able to input my middle name&/ do
+And /^I should be able to input my first name$/ do
   pending
 end
 
-But /^My confirmation of password should be same with the password&/ do
+And /^I should be able to input my middle name$/ do
   pending
 end
 
-And /^I should be able to chose my gender&/ do
+And /^I should be able to input my last name$/ do
   pending
 end
 
-And /^I should be able to input my last name&/ do
-  pending
-end
-
-And /^I should be able to click "submit" button after filling all fields&/ do
+And /^I should be able to click "submit" button after filling all fields$/ do
   pending
 end
