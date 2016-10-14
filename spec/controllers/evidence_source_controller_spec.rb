f1cc23884709ba9ef18a698218e84cb16d3bb346
Rex Wang <rex.wang.starry@gmail.com>
2016-10-14 16:58:39 +1300

    amendment of evidence_source_controller_spec

--- spec/controllers/evidence_source_controller_spec.rb
+++ spec/controllers/evidence_source_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe EvidenceSourcesController, type: :controller do
-  describe "GET #new_submitted" do
+
+  describe "moderator GET #new_submitted" do
   	it "returns http success" do
-      sign_in :username
+      sign_in 'moderator@example.com'
 
       get :new_submitted
       expect(response).to have_http_status(:success)
+      expect(response).to render_template("new_submitted")
     end
   end
+
+  describe "analyst GET #accepted" do
+  	it "return http success" do
+  	  sign_in "analyst@example.com"
+  	  get :accepted
+  	  expect(response).to have_http_status(:success)
+  	  expect(response).to render_template(:accepted)
+  	end
+  end
+
+  describe "moderator GET #rejected" do
+  	it "return http success" do
+  	  sign_in "moderator@example.com"
+  	  get :rejected
+  	  expect(response).to have_http_status(:success)
+  	  expect(response).to render_template(:rejected)
+  	end
+  end
+
 end
