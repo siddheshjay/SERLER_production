5f46feb2400bb66e08a2c8ffabde74674d61c359
Rex Wang <rex.wang.starry@gmail.com>
2016-10-15 15:12:32 +1300

    evidence_source_controller_spec

--- spec/controllers/evidence_source_controller_spec.rb
+++ spec/controllers/evidence_source_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe EvidenceSourcesController, type: :controller do
 
   describe "moderator GET #new_submitted" do
-  	it "returns http success" do
+  	it "returns http success and renders new_submitted template for moderator" do
       sign_in 'moderator@example.com'
 
       get :new_submitted
@@ -12,22 +12,175 @@ RSpec.describe EvidenceSourcesController, type: :controller do
     end
   end
 
-  describe "analyst GET #accepted" do
-  	it "return http success" do
+  describe "GET #accepted" do
+  	it "returns http success and renders the accepted template for analyst" do
   	  sign_in "analyst@example.com"
+  	  get :accepted
+  	  expect(response).to have_http_status(:success)
+  	  expect(response).to render_template(:accepted)
+  	end
+
+  	it "returns http success and renders the accepted template for administrator" do
+  	  sign_in "admin@example.com"
   	  get :accepted
   	  expect(response).to have_http_status(:success)
   	  expect(response).to render_template(:accepted)
   	end
   end
 
-  describe "moderator GET #rejected" do
-  	it "return http success" do
+  describe "GET #rejected" do
+  	it "returns http success and renders the rejected template for moderator" do
   	  sign_in "moderator@example.com"
+  	  get :rejected
+  	  expect(response).to have_http_status(:success)
+  	  expect(response).to render_template(:rejected)
+  	end
+
+  	it "returns http success and renders the rejected template for administrator" do
+  	  sign_in "admin@example.com"
   	  get :rejected
   	  expect(response).to have_http_status(:success)
   	  expect(response).to render_template(:rejected)
   	end
   end
 
-end
+  describe "POST #create evidence source" do
+  	it "input new evidence source" do
+  		sign_in "user@example.com"
+
+  		post :create, :evidence_source => {
+  			:page => '100-108',
+  			:title => 'TITLE OF THE ARTICLE',
+  			'author[]' => ['J. Wang', 'Rex Wang'],
+  		}
+
+  		expect(response).to have_http_status(302)
+   	end
+
+  	# it "" do
+  	# 	expect {
+  	# 		post :createwhat,
+  	# 		evidence_source: {
+  	# 			:page => '200-250',
+  	# 			:title => 'ANOTHER ARTICLE',
+  	# 			'author[]' => ['J. Wang', 'Rex Wang'],
+  	# 		}
+  	# 	}.to change(EvidenceSource, :count).by(1)
+  	# end
+
+  end
+
+  describe "POST #research_design" do
+  	it "update research_design" do
+  	  sign_in "user@example.com", :user
+	  post :create, :evidence_source => {
+		:page => '100-108',
+		:title => 'TITLE OF THE ARTICLE',
+		'author[]' => ['J. Wang', 'Rex Wang'],
+	  }
+
+  	  sign_in "analyst@example.com", :analyst
+
+  	  esid = EvidenceSource.limit(1)[0].id
+
+  	  post :research_design, :research_design => {
+  	  	:evidence_source_id => esid,
+  	  	:aim => 'the aim',
+  	  	:participants => '',
+  	  	:metrics => '',
+  	  }
+
+  	  expect(response).to have_http_status(302)
+  	end
+  end
+
+  describe "POST #evidence_item"
+    it "create new evience_item" do
+      sign_in "user@example.com", :user
+	  post :create, :evidence_source => {
+		:page => '100-108',
+		:title => 'TITLE OF THE ARTICLE',
+		'author[]' => ['J. Wang', 'Rex Wang'],
+	  }
+
+      sign_in "analyst@example.com", :analyst
+      esid = EvidenceSource.limit(1)[0].id
+
+      post :evidence_item, :evidence_item => {
+      	:esid => esid,
+      	:eiid => '-1',
+      	:benefit => 'benefit',
+      	:result => 'result',
+      	:ctx_where => 'context-where',
+      	:ctx_when => 'context-when',
+      	:ctx_how => 'context-how',
+      	:ctx_whom => 'context-whom',
+      	:ctx_what => 'context-what',
+
+      	'method[]' => '[0]',
+
+      	:submit => 'submit',
+
+        :integrity => '3',
+        :rating_tenth => '30',
+      }
+
+      expect(response).to have_http_status(302)
+    end
+
+    it "create draft evience_item and re-edit it" do
+      sign_in "user@example.com", :user
+	  post :create, :evidence_source => {
+		:page => '100-108',
+		:title => 'TITLE OF THE ARTICLE',
+		'author[]' => ['J. Wang', 'Rex Wang'],
+	  }
+
+      sign_in "analyst@example.com", :analyst
+      esid = EvidenceSource.limit(1)[0].id
+
+      post :evidence_item, :evidence_item => {
+      	:esid => esid,
+      	:eiid => '-1',
+      	:benefit => 'benefit',
+      	:result => 'result',
+      	:ctx_where => 'context-where',
+      	:ctx_when => 'context-when',
+      	:ctx_how => 'context-how',
+      	:ctx_whom => 'context-whom',
+      	:ctx_what => 'context-what',
+
+      	'method[]' => '[0]',
+
+      	:draft => 'draft',
+
+        :integrity => '3',
+        :rating_tenth => '30',
+      }
+
+      expect(response).to have_http_status(302)
+
+      eiid = G2EvidenceItem.limit(1)[0].id
+      post :evidence_item, :evidence_item => {
+      	:esid => esid,
+      	:eiid => eiid,
+      	:benefit => 'benefit',
+      	:result => 'result',
+      	:ctx_where => 'context-where',
+      	:ctx_when => 'context-when',
+      	:ctx_how => 'context-how',
+      	:ctx_whom => 'context-whom',
+      	:ctx_what => 'context-what',
+
+      	'method[]' => '[0]',
+
+      	:draft => 'draft',
+
+        :integrity => '3',
+        :rating_tenth => '30',
+      }
+
+    end
+  end
+
+# end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -3,13 +3,25 @@
 # see https://github.com/plataformatec/devise/wiki/How-To:-Stub-authentication-in-controller-specs
 
 module ControllerHelpers
-  def sign_in(user = double('user'))
-    if user.nil?
+  def sign_in(user_email, role = :user)
+    if user_email.nil?
       allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
       allow(controller).to receive(:current_user).and_return(nil)
+
     else
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      allow(controller).to receive(:current_user).and_return(user)
+
+      @fake_user = User.new({
+        email: user_email, password: "123456", password_confirmation: "123456",
+        reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1,
+        current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10",
+        current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", roles_mask: 0,
+        first_name: "Fistname", middle_name: "no-middle-name", last_name: "Lastname", 
+      });
+      @fake_user.roles= [role]
+      @fake_user.save!
+
+      allow(request.env['warden']).to receive(:authenticate!).and_return(@fake_user)
+      allow(controller).to receive(:current_user).and_return(@fake_user)
     end
   end
 end
